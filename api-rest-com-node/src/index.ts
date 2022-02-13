aaac49dc9f82fa68b936562330c59746e88d4a29
Kenia <keniacunha100@gmail.com>
2022-02-13 17:00:34 -0300

    sei la

--- api-rest-com-node/src/index.ts
+++ api-rest-com-node/src/index.ts
@@ -1,1 +1,22 @@
-console.log ("Executei !")
+import express,{Request, Response, NextFunction} from 'express';
+import usersRoute from './routes/users.route';
+
+const app = express();
+//config da aplicação
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//config de rotas
+app.use(usersRoute);
+
+app.get('/status',(req: Request, res: Response, next:NextFunction) =>{
+    res.status(200).send({ foo: 'Sucesso Total!!'});
+});
+
+//inicializador do servidor
+app.listen(3000, () =>{
+    console.log('Aplicação executando na porta 3000!');
+});
+
+
+    
---
+++ api-rest-com-node/src/routes/users.route.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Response, Router, Request } from "express";
+import { StatusCodes } from "http-status-codes";
+
+const usersRoute = Router();
+
+//get/users
+//get/users/:uuid
+//post/users
+//put/users/:uuid
+//delete/users/:uuid
+
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
+    const users = [{userName: 'Kenia'}];
+    res.status(StatusCodes.OK).send(users);
+});
+
+usersRoute.get('/users/:uuid', (req: Request<{uuid:string}>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    res.status(StatusCodes.OK).send({uuid});
+});
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+    const newUser = req.body;
+    console.log(req.body);
+    res.status(StatusCodes.CREATED).send(newUser);
+});
+
+usersRoute.put('/users/:uuid', (req: Request<{uuid:string}>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body;
+    modifiedUser.uuid = uuid;
+
+    res.status(StatusCodes.OK).send({uuid});
+});
+
+usersRoute.delete('/users/:uuid', (req: Request<{uuid:string}>, res: Response, next: NextFunction) => {
+    res.sendStatus(StatusCodes.OK);
+});
+
+export default usersRoute;
+
